151319dc651653d5a30dc9be266c1c64a1decf45
grafana2018 <35925741+grafana2018@users.noreply.github.com>
2019-02-22 23:59:49 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,33 +1,10 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = "us-east-1"
   access_key = "AKIAIFUKXZFPED5W3HHQ"
   secret_key = "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"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
-}
-
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+resource "aws_instance" "MQTTStest" {
+  ami = "ami-0080e4c5bc078760e"
   instance_type = "t2.micro"
-
-  tags {
-    Name = "HelloWorld"
-  }
-}
-output "ip"{
-value= "${aws_instance.web.public_ip}"
 }
