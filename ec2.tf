34d53a76ea67faa794341b53a43ca566859aec18
grafana2018 <35925741+grafana2018@users.noreply.github.com>
2019-02-24 14:49:41 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,11 @@
 provider "aws" {
   version = "~> 1.60"
   region = "us-east-1"
-  profile = "default"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
 
 resource "aws_instance" "MQTTStest" {
-  ami = "ami-0080e4c5bc078760e"
+  ami = "ami-035be7bafff33b6b6"
   instance_type = "t2.micro"
 }
