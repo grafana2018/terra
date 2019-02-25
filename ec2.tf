dd4d549dbf24e1dd23010655cc9320b2d87db123
grafana2018 <35925741+grafana2018@users.noreply.github.com>
2019-02-25 18:30:40 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,17 @@ provider "aws" {
   profile = "default"
 }
 
-resource "aws_instance" "MQTTStest" {
+resource "aws_instance" "MQTTS" {
   ami = "ami-035be7bafff33b6b6"
   instance_type = "t2.micro"
-}
+  subnet_id = "subnet-7329425d"
+  iam_instance_profile = "ec2-full-access"
+  availability_zone = "us-east-1d"
+  security_groups = ["sg-ea1caeae"]
+  associate_public_ip_address = "true"
+  key_name = "cherrysree1"
+
+  tags {
+    Name = "/TEST"
+    Project = "testing"
+  }
