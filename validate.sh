4b4c4e18223cb449fed5ac55d696bd87388a41ea
grafana2018 <35925741+grafana2018@users.noreply.github.com>
2019-03-08 18:29:12 +0530

    Create validate.sh

---
+++ validate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ID=`terraform output INSTANCEID`
+echo $ID
+status=`aws ec2 describe-instances --filters --instance-ids=$ID --region us-east-1 --query Reservations[].Instances[].State.Name --output text`
+if [[ status -eq 'running' ]]; then
+echo "$ID Server is running"
+else
+echo "$ID is not running"
+fi
+
+mkdir -p /tmp/$ID
+cp -R ./terra /tmp/$ID
